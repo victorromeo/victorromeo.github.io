00dbf9d168e6ef8b36b2c0764b3a363638a74ac5
victorromeo <victorromeo@github.com>
2021-02-16 16:00:37 +1100

    local serve

--- serve.sh
+++ serve.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+JEKYLL_VERSION=4.2.0
+docker container rm myblog
 docker run --name myblog --volume="$PWD:/srv/jekyll" -p 4000:4000 -it jekyll/jekyll:$JEKYLL_VERSION jekyll serve --watch --drafts
